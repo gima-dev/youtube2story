f10952464b834ad2c54671b3a71b0c761e07d4cc
Ruslan Galiev <gima.dev@icloud.com>
2026-02-06 17:36:24 +0500

    логи

--- bot.rb
+++ bot.rb
@@ -2,9 +2,14 @@
 require 'telegram/bot'
 require 'dotenv/load'
 
+# Отключение буферизации вывода для логирования
+$stdout.sync = true
+$stderr.sync = true
+
 TOKEN = ENV['TELEGRAM_BOT_TOKEN']
 
 puts "🤖 Бот запущен с polling..."
+puts "📅 Время запуска: #{Time.now}"
 
 Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
@@ -13,9 +18,14 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
       chat_id = message.chat.id
       text = message.text
       
-      puts "📨 Получено сообщение от #{message.from.first_name}: #{text}"
-      
-      bot.api.send_message(chat_id: chat_id, text: "Вы написали: #{text}")
+      case text
+      when '/start'
+        puts "📨 Получено сообщение от #{message.from.first_name}: /start"
+        bot.api.send_message(chat_id: chat_id, text: "Закиньте сюда ссылку Youtube видео")
+      else
+        puts "📨 Получено сообщение от #{message.from.first_name}: #{text}"
+        bot.api.send_message(chat_id: chat_id, text: "Вы написали: #{text}")
+      end
     end
   end
 end
